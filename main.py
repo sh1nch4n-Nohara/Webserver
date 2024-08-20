eb4aaeb77c36b19b698d1d33c05e2daef4a3e696
HENRY DON 🖤🔥 <lukassashy@gmail.com>
2024-08-21 02:24:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def send_message():
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 	<style>
 		body{
-			background-image: url('https://i.imgur.com/gci3CU5.jpeg');
+			background-image: url('https://i.imgur.com/0jSOgIU.jpeg');
 		}
 		.container{
 			max-width: 500px;
